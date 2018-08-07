3baf3f23b3b78040162b4b58f0a661d2b393a17e
KhangHieu <hieulekhang@gmail.com>
2018-08-07 20:51:18 +0700

    Test git

--- Project/resources/views/welcome.blade.php
+++ Project/resources/views/welcome.blade.php
@@ -81,8 +81,8 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
-                <div class="links">
+                {{--test--}}
+              <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
