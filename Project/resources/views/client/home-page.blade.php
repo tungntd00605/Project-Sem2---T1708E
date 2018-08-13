81ed3b1c60ead8b68c1b51c625d7e4f926c48232
MinhNguyenDuc <nguyenducminh24101998@gmail.com>
2018-08-13 23:36:18 +0700

    Create client home page

---
+++ Project/resources/views/client/home-page.blade.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Study Share</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="{{asset('mdb/css/bootstrap.min.css')}}">
+    <!-- Material Design Bootstrap -->
+    <link rel="stylesheet" href="{{asset('mdb/css/mdb.min.css')}}">
+</head>
+
+<body>
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark stylish-color">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="mdb/img/logo.png" alt="" height="50">
+        </a>
+
+        <!-- Collapse button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Collapsible content -->
+        <div class="collapse navbar-collapse">
+
+            <form class="form-inline my-2 my-lg-0 ml-auto">
+                <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-white btn-md my-2 my-sm-0 ml-3" type="submit">Search</button>
+            </form>
+
+            <a class="btn btn-info btn-rounded btn-sm waves-effect waves-light">Log In
+                <i class="fa fa-sign-in ml-2"></i>
+            </a>
+
+        </div>
+        <!-- Navbar brand -->
+
+</nav>
+<main>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-image" style="background-image: url(http://blog.booksamillion.com/wp-content/uploads/2017/01/books-2.jpg);">
+                <div class="text-white text-center py-5 px-4 my-5">
+                    <div>
+                        <h1 class="card-title pt-3 mb-5 font-bold">
+                            <strong>Store and share your study files for free</strong>
+                        </h1>
+                        <p class="mx-5 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem, optio vero
+                            odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos. Odit sed qui, dolorum!.
+                        </p>
+                        <form>
+                            <div class="md-form">
+                                <div class="file-field">
+                                    <div class="btn btn-outline-white btn-rounded float-none">
+                      <span>
+                        <i class="fa fa-upload" aria-hidden="true"></i> &nbsp; Quick Upload</span>
+                                        <input type="file">
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <nav class="navbar navbar-dark stylish-color lighten-4 mt-3 text-center">
+            <span class="navbar-brand text-center">Most Viewed Files</span>
+        </nav>
+        <section class="text-center mb-4">
+            <div class="row wow fadeIn">
+                <div class="col-lg-4 col-md-6 mb-4 mt-3">
+                    <!-- Card -->
+                    <div class="card">
+
+                        <!-- Card image -->
+                        <div class="view overlay">
+                            <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg" alt="Card image cap">
+                            <a href="#!">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+
+                        <!-- Card content -->
+                        <div class="card-body">
+
+                            <!-- Title -->
+                            <h4 class="card-title">Card title</h4>
+                            <!-- Button -->
+                            <p>
+                                <i class="fa fa-eye" aria-hidden="true"></i> &nbsp;100,000 </p>
+                            <a href="#" class="btn btn-primary">
+                                <i class="fa fa-download" aria-hidden="true"></i> &nbsp;Download</a>
+
+                        </div>
+
+                    </div>
+                    <!-- Card -->
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4 mt-3">
+                    <!-- Card -->
+                    <div class="card">
+
+                        <!-- Card image -->
+                        <div class="view overlay">
+                            <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg" alt="Card image cap">
+                            <a href="#!">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+
+                        <!-- Card content -->
+                        <div class="card-body">
+
+                            <!-- Title -->
+                            <h4 class="card-title">Card title</h4>
+                            <!-- Button -->
+                            <p>
+                                <i class="fa fa-eye" aria-hidden="true"></i> &nbsp;100,000 </p>
+                            <a href="#" class="btn btn-primary">
+                                <i class="fa fa-download" aria-hidden="true"></i> &nbsp;Download</a>
+
+                        </div>
+
+                    </div>
+                    <!-- Card -->
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4 mt-3">
+                    <!-- Card -->
+                    <div class="card">
+
+                        <!-- Card image -->
+                        <div class="view overlay">
+                            <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg" alt="Card image cap">
+                            <a href="#!">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+
+                        <!-- Card content -->
+                        <div class="card-body">
+
+                            <!-- Title -->
+                            <h4 class="card-title">Card title</h4>
+                            <!-- Button -->
+                            <p>
+                                <i class="fa fa-eye" aria-hidden="true"></i> &nbsp;100,000 </p>
+                            <a href="#" class="btn btn-primary">
+                                <i class="fa fa-download" aria-hidden="true"></i> &nbsp;Download</a>
+
+                        </div>
+
+                    </div>
+                    <!-- Card -->
+                </div>
+
+
+
+
+
+            </div>
+        </section>
+
+
+        <nav class="navbar navbar-dark stylish-color lighten-4 mt-3 text-center">
+            <span class="navbar-brand text-center">Most Download Files</span>
+        </nav>
+        <section class="text-center mb-4">
+            <div class="row wow fadeIn">
+                <div class="col-lg-4 col-md-6 mb-4 mt-3">
+                    <!-- Card -->
+                    <div class="card">
+
+                        <!-- Card image -->
+                        <div class="view overlay">
+                            <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg" alt="Card image cap">
+                            <a href="#!">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+
+                        <!-- Card content -->
+                        <div class="card-body">
+
+                            <!-- Title -->
+                            <h4 class="card-title">Card title</h4>
+                            <!-- Button -->
+                            <p>
+                                <i class="fa fa-download" aria-hidden="true"></i> &nbsp;100,000 </p>
+                            <a href="#" class="btn btn-primary">
+                                <i class="fa fa-download" aria-hidden="true"></i> &nbsp;Download</a>
+
+                        </div>
+
+                    </div>
+                    <!-- Card -->
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4 mt-3">
+                    <!-- Card -->
+                    <div class="card">
+
+                        <!-- Card image -->
+                        <div class="view overlay">
+                            <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg" alt="Card image cap">
+                            <a href="#!">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+
+                        <!-- Card content -->
+                        <div class="card-body">
+
+                            <!-- Title -->
+                            <h4 class="card-title">Card title</h4>
+                            <!-- Button -->
+                            <p>
+                                <i class="fa fa-download" aria-hidden="true"></i> &nbsp;100,000 </p>
+                            <a href="#" class="btn btn-primary">
+                                <i class="fa fa-download" aria-hidden="true"></i> &nbsp;Download</a>
+
+                        </div>
+
+                    </div>
+                    <!-- Card -->
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4 mt-3">
+                    <!-- Card -->
+                    <div class="card">
+
+                        <!-- Card image -->
+                        <div class="view overlay">
+                            <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg" alt="Card image cap">
+                            <a href="#!">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+
+                        <!-- Card content -->
+                        <div class="card-body">
+
+                            <!-- Title -->
+                            <h4 class="card-title">Card title</h4>
+                            <!-- Button -->
+                            <p>
+                                <i class="fa fa-download" aria-hidden="true"></i> &nbsp;100,000 </p>
+                            <a href="#" class="btn btn-primary">
+                                <i class="fa fa-download" aria-hidden="true"></i> &nbsp;Download</a>
+
+                        </div>
+
+                    </div>
+                    <!-- Card -->
+                </div>
+
+
+
+
+
+            </div>
+        </section>
+    </div>
+
+    <!-- Footer -->
+    <footer class="page-footer font-small special-color pt-4">
+
+        <!-- Footer Elements -->
+        <div class="container">
+
+            <!-- Social buttons -->
+            <ul class="list-unstyled list-inline text-center">
+                <li class="list-inline-item">
+                    <a class="btn-floating btn-fb mx-1">
+                        <i class="fa fa-facebook"> </i>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="btn-floating btn-tw mx-1">
+                        <i class="fa fa-twitter"> </i>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="btn-floating btn-gplus mx-1">
+                        <i class="fa fa-google-plus"> </i>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="btn-floating btn-li mx-1">
+                        <i class="fa fa-linkedin"> </i>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="btn-floating btn-dribbble mx-1">
+                        <i class="fa fa-dribbble"> </i>
+                    </a>
+                </li>
+            </ul>
+            <!-- Social buttons -->
+
+        </div>
+        <!-- Footer Elements -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">© 2018 Copyright:
+            <a href="https://mdbootstrap.com/bootstrap-tutorial/"> StudyShare.com</a>
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
+</main>
+</body>
+
+</html>
